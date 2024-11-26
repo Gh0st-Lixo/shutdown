123e226d44368f8b85897fbfda5523ecb60f7df1
Ghost Lixo <martinsclaudiojunior@gmail.com>
2024-11-25 23:01:57 -0300

    Delete teste.cpp

--- teste.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream> //Código de teste
-int main ()
-{   std::cout << "Essa porra tinha que executar um desligamento e não uma mensagem";
-    system ("shutdown -s -t 500"); //Deveria agendar um desligamento
-    //Antes de eu ter feito um código mais complexo isso funcionava de boa, agora não está mais!
-    return 0;
-}
